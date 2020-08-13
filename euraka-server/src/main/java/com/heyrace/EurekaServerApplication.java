7515dd0808cd7411a967658ac2947afcedb0e7f1
LiHbeen <51393642+LiHbeen@users.noreply.github.com>
2020-08-13 10:18:19 +0800

    1、初始化gateway，调试成功

--- euraka-server/src/main/java/com/heyrace/EurekaServerApplication.java
+++ euraka-server/src/main/java/com/heyrace/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class CourseApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourseApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
